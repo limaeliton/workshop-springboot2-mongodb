fd43e52779d2aa1c36f193e9f4987f864f3b5240
Lima Eliton <elitonlima2@hotmail.com>
2022-12-16 18:06:09 -0300

    Consulta simples com query methods

--- src/main/java/com/elitonlima/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/elitonlima/workshopmongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.elitonlima.workshopmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.elitonlima.workshopmongodb.domain.Post;
 
 // é uma interface
 @Repository // para salvar e fazer operações no banco de dados
+
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/elitonlima/workshopmongodb/services/PostService.java
+++ src/main/java/com/elitonlima/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.elitonlima.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	// Consulta simples com query methods Para Mongo
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
